a4aa40c4c65cfd8215e84824bcc5165cf0221dd0
bella52057 <bellabartol04@gmail.com>
2018-07-25 14:59:40 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(fish)
-  if fish %3 == 0 && fish %5 == 0 
+  if fish % 3 == 0 && fish % 5 == 0 
     puts "FizzBuzz"
-  elsif fish %3 == 0 
+  elsif fish % 3 == 0 
     puts "Fizz"
-  elsif fish %5 == 0
+  elsif fish % 5 == 0
     puts "Buzz" 
   else
     puts "fish"
